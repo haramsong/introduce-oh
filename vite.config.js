e84d1642adb5686939d9b957887e092a19eae70f
haramsong <alakazam1324@naver.com>
2024-11-25 11:19:55 +0900

    파비콘 추가

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import path from "path";
 export default defineConfig({
     base: './',
     plugins: [vitePluginGLSL()],
+    build: {
+        emptyOutDir: true, // dist 폴더를 빌드 전에 비움
+    },
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
